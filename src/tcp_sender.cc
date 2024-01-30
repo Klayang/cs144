7da91245cec5603df9f8f036cd2c22352080762a
Shijie Yang <891363540@qq.com>
2024-01-30 05:46:51 +0000

    the 1st version passing autograder

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -1,53 +1,192 @@
 #include "tcp_sender.hh"
 #include "tcp_config.hh"
+#include <functional>
+#include <algorithm>
 
 #include <random>
 
 using namespace std;
 
 /* TCPSender constructor (uses a random ISN if none given) */
-TCPSender::TCPSender( uint64_t initial_RTO_ms, optional<Wrap32> fixed_isn )
-  : isn_( fixed_isn.value_or( Wrap32 { random_device()() } ) ), initial_RTO_ms_( initial_RTO_ms )
-{}
+TCPSender::TCPSender( uint64_t initial_RTO_ms, optional<Wrap32> fixed_isn ):
+    isn_( fixed_isn.value_or( Wrap32 { random_device()() } ) ), left_edge_of_window(0), right_edge_of_window(1),
+    initial_RTO_ms_( initial_RTO_ms ), current_RTO_ms(initial_RTO_ms), timer(0), isTimerOn(false),
+    num_of_consecutive_retransmissions(0), toBeSentSegments(vector<TCPSenderMessage>()),
+    outstandingSegments(vector<TCPSenderMessage>()), SYN(true), FIN(false)
+{
+}
 
 uint64_t TCPSender::sequence_numbers_in_flight() const
 {
-  // Your code here.
-  return {};
+    uint64_t res = 0;
+    for (auto segment: outstandingSegments) {
+        res += segment.sequence_length();
+    }
+    return res;
 }
 
 uint64_t TCPSender::consecutive_retransmissions() const
 {
-  // Your code here.
-  return {};
+    return num_of_consecutive_retransmissions;
 }
 
-optional<TCPSenderMessage> TCPSender::maybe_send()
-{
-  // Your code here.
-  return {};
+void TCPSender::handleLookAheadCase(const std::string& data, Reader& outbound_stream) {
+    TCPSenderMessage segment;
+    // set seqno field of TCPSenderMessage
+    segment.seqno = Wrap32::wrap(left_edge_of_window, isn_);
+    if (SYN) {
+        segment.SYN = true;
+        SYN = false;
+    }
+    else if (!data.empty()) {
+        segment.payload = data.substr(0, 1);
+    }
+    else {
+        segment.FIN = true;
+        FIN = true;
+    }
+
+    outstandingSegments.push_back(segment);
+    toBeSentSegments.push_back(segment);
+    left_edge_of_window += 1;
+    outbound_stream.pop(1);
 }
 
 void TCPSender::push( Reader& outbound_stream )
 {
-  // Your code here.
-  (void)outbound_stream;
+    // the reader has been closed
+    if (FIN) {
+        return;
+    }
+
+    // no data in the reader
+    if (!SYN && outbound_stream.bytes_buffered() == 0 && !outbound_stream.is_finished()) {
+        return;
+    }
+
+    // no window size
+    if (left_edge_of_window > right_edge_of_window) {
+        return;
+    }
+
+    // shouldn't send that 1-byte segment since there are to-be-sent segments
+    if (left_edge_of_window == right_edge_of_window && !outstandingSegments.empty()) {
+        return;
+    }
+
+    uint64_t windowSpace = right_edge_of_window - left_edge_of_window;
+    uint16_t availableBytes = min(outbound_stream.bytes_buffered() + SYN, windowSpace);
+    string data = {outbound_stream.peek().begin(), outbound_stream.peek().end()};
+    if (availableBytes == 0) {
+        handleLookAheadCase(data, outbound_stream);
+        return;
+    }
+
+    size_t dataLength = availableBytes;
+    if (SYN) dataLength -= 1;
+
+    data = data.substr(0, dataLength);
+    outbound_stream.pop(dataLength);
+    availableBytes = min(availableBytes + outbound_stream.is_finished(), (int)windowSpace);
+
+    int numOfSegments = dataLength / TCPConfig::MAX_PAYLOAD_SIZE;
+    if (dataLength % TCPConfig::MAX_PAYLOAD_SIZE == 0 && numOfSegments > 0) {
+        numOfSegments -= 1;
+    }
+    for (int i = 0; i <= numOfSegments; ++i) {
+        TCPSenderMessage segment;
+
+        // set seqno field of TCPSenderMessage
+        segment.seqno = Wrap32::wrap(left_edge_of_window, isn_);
+
+        // set SYN field of TCPSenderMessage
+        segment.SYN = SYN;
+        if (SYN) {
+            SYN = false;
+            availableBytes -= 1;
+        }
+
+        // set payload field of TCPSenderMessage
+        uint16_t bytesLeftInData = data.size() - i * TCPConfig::MAX_PAYLOAD_SIZE;
+        uint16_t  numOfBytes = min(min(uint16_t (TCPConfig::MAX_PAYLOAD_SIZE), availableBytes), bytesLeftInData);
+        segment.payload = data.substr(i * TCPConfig::MAX_PAYLOAD_SIZE, numOfBytes);
+
+        // get available bytes left after loading the current payload
+        availableBytes -= numOfBytes;
+
+        // set FIN field of TCPSenderMessage
+        if (outbound_stream.is_finished() && availableBytes > 0 && i == numOfSegments) {
+            FIN = true;
+            segment.FIN = true;
+            availableBytes -= 1;
+        }
+
+        // put the segment in the buffer
+        outstandingSegments.push_back(segment);
+        toBeSentSegments.push_back(segment);
+
+        // update left edge of window
+        left_edge_of_window += segment.sequence_length();
+    }
 }
 
-TCPSenderMessage TCPSender::send_empty_message() const
+optional<TCPSenderMessage> TCPSender::maybe_send()
 {
-  // Your code here.
-  return {};
+    if (toBeSentSegments.empty()) return nullopt;
+//    if (toBeSentSegments.size() < outstandingSegments.size() && left_edge_of_window > right_edge_of_window) return nullopt;
+    if (!isTimerOn) {
+        isTimerOn = true;
+        timer = 0;
+    }
+    TCPSenderMessage res = toBeSentSegments[0];
+    toBeSentSegments.erase(toBeSentSegments.begin());
+    return res;
 }
 
 void TCPSender::receive( const TCPReceiverMessage& msg )
 {
-  // Your code here.
-  (void)msg;
+    uint64_t ackno = 0;
+    if (msg.ackno.has_value()) {
+        ackno = msg.ackno.value().unwrap(isn_, left_edge_of_window);
+    }
+    if (ackno > left_edge_of_window) return;
+    uint16_t window = msg.window_size;
+    right_edge_of_window = max(right_edge_of_window, ackno + (uint64_t)window);
+    UpdateOutstandingSegmentsFunctor functor(*this, ackno);
+
+    // this variable is set to check if any outstanding segments have been acked
+    long unsigned int previous_num_of_outstanding_segments = outstandingSegments.size();
+    // remove all segments whose seqno are less than the receiver message one, which means, having been acked
+    outstandingSegments.erase(remove_if(outstandingSegments.begin(), outstandingSegments.end(), ref(functor)),
+                              outstandingSegments.end());
+    if (outstandingSegments.size() < previous_num_of_outstanding_segments) {
+        current_RTO_ms = initial_RTO_ms_;
+        timer = 0;
+        num_of_consecutive_retransmissions = 0;
+        if (outstandingSegments.empty()) {
+            isTimerOn = false;
+        }
+    }
 }
 
 void TCPSender::tick( const size_t ms_since_last_tick )
 {
-  // Your code here.
-  (void)ms_since_last_tick;
+    if (!isTimerOn) return;
+    timer += ms_since_last_tick;
+    if (timer >= current_RTO_ms) {
+        if (left_edge_of_window <= right_edge_of_window) {
+            current_RTO_ms *= 2;
+            num_of_consecutive_retransmissions += 1;
+        }
+        TCPSenderMessage segment = outstandingSegments[0];
+        toBeSentSegments.insert(toBeSentSegments.begin(), segment);
+        timer = 0;
+    }
+}
+
+TCPSenderMessage TCPSender::send_empty_message() const
+{
+    TCPSenderMessage segment;
+    segment.seqno = Wrap32::wrap(left_edge_of_window, isn_);
+    return segment;
 }
--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -1,34 +1,62 @@
 #pragma once
 
+#include <string>
 #include "byte_stream.hh"
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
 
 class TCPSender
 {
-  Wrap32 isn_;
-  uint64_t initial_RTO_ms_;
+    Wrap32 isn_;
+    uint64_t left_edge_of_window;
+    uint64_t right_edge_of_window;
+    uint64_t initial_RTO_ms_;
+    uint64_t current_RTO_ms;
+    uint64_t timer;
+    bool isTimerOn;
+    uint8_t num_of_consecutive_retransmissions;
+    std::vector<TCPSenderMessage> toBeSentSegments;
+    std::vector<TCPSenderMessage> outstandingSegments;
+    bool SYN;
+    bool FIN;
+    void handleLookAheadCase(const std::string& data, Reader& outbound_stream);
+    class UpdateOutstandingSegmentsFunctor {
+        TCPSender& sender_;
+        const uint64_t& ackno_; // this is the seqno from the tcp receiver message
+    public:
+        explicit UpdateOutstandingSegmentsFunctor(TCPSender& sender, const uint64_t& ackno): sender_(sender), ackno_(ackno) {}
+        bool operator()(const TCPSenderMessage& segment) {
+            uint64_t current_outstanding_seqno = segment.seqno.unwrap(sender_.isn_, sender_.left_edge_of_window)
+                    + segment.sequence_length();
 
+            if (current_outstanding_seqno > ackno_) {
+                return false;
+            }
+
+//            sender_.left_edge_of_window = current_outstanding_seqno;
+            return true;
+        }
+    };
 public:
-  /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
-  TCPSender( uint64_t initial_RTO_ms, std::optional<Wrap32> fixed_isn );
+    /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
+    TCPSender( uint64_t initial_RTO_ms, std::optional<Wrap32> fixed_isn );
 
-  /* Push bytes from the outbound stream */
-  void push( Reader& outbound_stream );
+    /* Push bytes from the outbound stream */
+    void push( Reader& outbound_stream );
 
-  /* Send a TCPSenderMessage if needed (or empty optional otherwise) */
-  std::optional<TCPSenderMessage> maybe_send();
+    /* Send a TCPSenderMessage if needed (or empty optional otherwise) */
+    std::optional<TCPSenderMessage> maybe_send();
 
-  /* Generate an empty TCPSenderMessage */
-  TCPSenderMessage send_empty_message() const;
+    /* Generate an empty TCPSenderMessage */
+    TCPSenderMessage send_empty_message() const;
 
-  /* Receive an act on a TCPReceiverMessage from the peer's receiver */
-  void receive( const TCPReceiverMessage& msg );
+    /* Receive an act on a TCPReceiverMessage from the peer's receiver */
+    void receive( const TCPReceiverMessage& msg );
 
-  /* Time has passed by the given # of milliseconds since the last time the tick() method was called. */
-  void tick( uint64_t ms_since_last_tick );
+    /* Time has passed by the given # of milliseconds since the last time the tick() method was called. */
+    void tick( uint64_t ms_since_last_tick );
 
-  /* Accessors for use in testing */
-  uint64_t sequence_numbers_in_flight() const;  // How many sequence numbers are outstanding?
-  uint64_t consecutive_retransmissions() const; // How many consecutive *re*transmissions have happened?
+    /* Accessors for use in testing */
+    uint64_t sequence_numbers_in_flight() const;  // How many sequence numbers are outstanding?
+    uint64_t consecutive_retransmissions() const; // How many consecutive *re*transmissions have happened?
 };
